e4cbfb2a2d9f69f9caf95fad60f63eaf6b90b213
anabolic-angel <35975987+anabolic-angel@users.noreply.github.com>
2020-06-01 07:30:01 -0400

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,16 @@
 def main():
    print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes)
 
 if __name__== "__main__":
   main()
   
-python get-quote.py
+python3 get-quote.py
+
+
 
